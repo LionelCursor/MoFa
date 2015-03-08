c930dada0fac8490c0d39fffe102e9defb391b62
LionelCursor <danxionglei@hustunique.com>
2015-03-08 15:32:38 +0800

    MoFaSeekBar

--- app/src/main/java/com/unique/mofaforhackday/view/MoFaSeekBar.java
+++ app/src/main/java/com/unique/mofaforhackday/view/MoFaSeekBar.java
@@ -1,12 +1,12 @@
 package com.unique.mofaforhackday.view;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 
-import android.widget.AbsSeekBar;
+import android.view.ViewTreeObserver;
 import android.widget.SeekBar;
-
-import com.unique.mofaforhackday.Utils.L;
 
 /**
  * Created by ldx on 2014/9/19.
@@ -15,20 +15,52 @@ public class MoFaSeekBar extends SeekBar {
 
     private OnSeekBarChangeListener mListener;
 
+    private boolean isDrawCustomBackground;
+    private Paint mBackgroundLittleRoundDotPaint;
+
+
     public MoFaSeekBar(Context context) {
-        super(context);
+        super(context, null);
+        init();
     }
 
     public MoFaSeekBar(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public MoFaSeekBar(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        init();
+    }
+
+    private void init(){
+        isDrawCustomBackground = true;
+        mBackgroundLittleRoundDotPaint = new Paint();
+        mBackgroundLittleRoundDotPaint.setAntiAlias(true);
+        mBackgroundLittleRoundDotPaint.setColor(0xffffffff);
+        mBackgroundLittleRoundDotPaint.setStyle(Paint.Style.FILL);
+        this.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                MoFaSeekBar.this.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        getMeasuredHeight();
+                    }
+                });
+            }
+        });
+    }
+
+
+
+    /** set if draw little circle for background*/
+    private void setDrawCustomBackground(boolean bool){
+        isDrawCustomBackground = bool;
     }
 
     @Override
-
     public synchronized void setProgress(int progress) {
         super.setProgress(progress);
 
@@ -47,4 +79,26 @@ public class MoFaSeekBar extends SeekBar {
     }
 
 
+    @Override
+    protected synchronized void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+
+
+    @Override
+    protected synchronized void onDraw(Canvas canvas) {
+        if (isDrawCustomBackground){
+            int width = getMeasuredWidth()-getPaddingLeft()-getPaddingRight();
+            canvas.save();
+            canvas.translate(getPaddingLeft(), getPaddingTop()+getMeasuredHeight()/2f);
+            for(int i= 0;i<10;i++){
+                canvas.drawCircle(0,0,10,mBackgroundLittleRoundDotPaint);
+                canvas.translate(width/10f,0);
+            }
+            canvas.drawCircle(0,0,10,mBackgroundLittleRoundDotPaint);
+            canvas.restore();
+        }
+        super.onDraw(canvas);
+    }
 }
